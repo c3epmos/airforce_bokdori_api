a11a1c4bd1192b535f4758cd47025fe52117519d
c3epmos <c3e.pmos@gmail.com>
2022-05-05 17:35:05 +0900

    로직 변경

--- airforce_bokdori_api/menu/views.py
+++ airforce_bokdori_api/menu/views.py
@@ -10,7 +10,7 @@ class MenuView(View):
             # TODO 1 : 비어 있는 param 가 없는 지 검증
             req_body = json.loads(request.body)
             param_dict = {}
-            param_dict['date'] = req_body.get('action').get('params').get('date')
+            param_dict['date'] = json.loads(req_body.get('action').get('params').get('date')).get('date')
             param_dict['cycle'] = req_body.get('action').get('params').get('cycle')
             param_dict['location'] = req_body.get('action').get('params').get('location')
             for key, value in param_dict.items():
@@ -18,24 +18,20 @@ class MenuView(View):
                     raise Exception(key)       
         except Exception as e:
             return JsonResponse({
-                        'error': '1',
-                        'data': {
-                            'message': f'{e} 입력 값이 없습니다!'
-                        }    
-                    } ,status=400)
+                        'version': '2',
+                        'data': {},
+                        } ,status=200)
         try:
             # TODO 2 : param 으로 3개 값 받기(date, cycle, location)
             menu_obj = Menu.objects.get(date=param_dict['date'], cycle=param_dict['cycle'], location=param_dict['location'])
         except Exception:
             return JsonResponse({
-                        'error': '1',
-                        'data': {
-                            'message': '조건에 맞는 메뉴가 없습니다!'
-                        },
-                        } ,status=500)
+                        'version': '2',
+                        'data': {},
+                        } ,status=200)
         # TODO 3 : 요청 param 에 일치하는 queryset 추출 후 json 응답
         return JsonResponse({
-            'error': '0',
+            'version': '2',
             'data': {
                 'menu1': menu_obj.menu1,
                 'menu2': menu_obj.menu2,
