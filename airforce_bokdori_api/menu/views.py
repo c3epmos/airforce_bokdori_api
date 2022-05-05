e4239ea9e30701d8e7d72d369d0f3722c4b95e60
c3epmos <c3e.pmos@gmail.com>
2022-05-05 16:42:09 +0900

    data 로 응답 포맷 수정

--- airforce_bokdori_api/menu/views.py
+++ airforce_bokdori_api/menu/views.py
@@ -19,20 +19,24 @@ class MenuView(View):
         except Exception as e:
             return JsonResponse({
                         'error': '1',
-                        'message': f'{e} 입력 값이 없습니다!'
-                        } ,status=400)
+                        'data': {
+                            'message': f'{e} 입력 값이 없습니다!'
+                        }    
+                    } ,status=400)
         try:
             # TODO 2 : param 으로 3개 값 받기(date, cycle, location)
             menu_obj = Menu.objects.get(date=param_dict['date'], cycle=param_dict['cycle'], location=param_dict['location'])
         except Exception:
             return JsonResponse({
                         'error': '1',
-                        'message': '조건에 맞는 메뉴가 없습니다!'
+                        'data': {
+                            'message': '조건에 맞는 메뉴가 없습니다!'
+                        },
                         } ,status=500)
         # TODO 3 : 요청 param 에 일치하는 queryset 추출 후 json 응답
         return JsonResponse({
             'error': '0',
-            'message': {
+            'data': {
                 'menu1': menu_obj.menu1,
                 'menu2': menu_obj.menu2,
                 'menu3': menu_obj.menu3,
