d178c8e37871950ca6439d2ce3ee3c0d2651824f
c3epmos <c3epmos@gmail.com>
2022-05-14 11:16:50 +0900

    데이터 수정으로 인한 commit

---
+++ airforce_bokdori_api/airforce_bokdori_api/menu/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Menu(models.Model):
+    cycle_choices = (
+        ('조식', '조식'),
+        ('중식', '중식'),
+        ('석식', '석식')
+    )
+    location_choices = (
+        ('칠성병사식당', '칠성병사식당'),
+        ('보라매병사식당', '보라매병사식당'),
+        ('독수리회관', '독수리회관'),
+        ('다미정', '다미정'),
+        ('보라매식당', '보라매식당')
+    )
+
+    date = models.DateField()
+    cycle = models.CharField(max_length=100, choices=cycle_choices)
+    location = models.CharField(max_length=100, choices=location_choices)
+    menu1 = models.CharField(max_length=100, null=True, blank=True)
+    menu2 = models.CharField(max_length=100, null=True, blank=True)
+    menu3 = models.CharField(max_length=100, null=True, blank=True)
+    menu4 = models.CharField(max_length=100, null=True, blank=True)
+    menu5 = models.CharField(max_length=100, null=True, blank=True)
---
+++ airforce_bokdori_api/airforce_bokdori_api/menu/views.py
@@ -0,0 +1,42 @@
+from django.views.generic import View
+from django.http import JsonResponse
+from menu.models import Menu
+import json
+
+# Create your views here.
+class MenuView(View):
+    def post(self, request):
+        try:
+            # TODO 1 : 비어 있는 param 가 없는 지 검증
+            req_body = json.loads(request.body)
+            param_dict = {}
+            param_dict['date'] = json.loads(req_body.get('action').get('params').get('date')).get('date')
+            param_dict['cycle'] = req_body.get('action').get('params').get('cycle')
+            param_dict['location'] = req_body.get('action').get('params').get('location')
+            for key, value in param_dict.items():
+                if value is None:
+                    raise Exception(key)
+        except Exception as e:
+            return JsonResponse({
+                        'version': '2',
+                        'data': {},
+                        } ,status=200)
+        try:
+            # TODO 2 : param 으로 3개 값 받기(date, cycle, location)
+            menu_obj = Menu.objects.get(date=param_dict['date'], cycle=param_dict['cycle'], location=param_dict['location'])
+        except Exception:
+            return JsonResponse({
+                        'version': '2',
+                        'data': {},
+                        } ,status=200)
+        # TODO 3 : 요청 param 에 일치하는 queryset 추출 후 json 응답
+        return JsonResponse({
+            'version': '2',
+            'data': {
+                'menu1': menu_obj.menu1,
+                'menu2': menu_obj.menu2,
+                'menu3': menu_obj.menu3,
+                'menu4': menu_obj.menu4,
+                'menu5': menu_obj.menu5,
+            }
+        })
